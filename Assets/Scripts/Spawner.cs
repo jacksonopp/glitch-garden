afbfd76c20a1c8326fba4cdd8a00716b510a0df4
Jackson Oppenheim <jacksonop@gmail.com>
2020-09-30 20:34:44 -0500

    initial push

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    [SerializeField] Attacker attackerPrefab;
+
+    bool spawn = true;
+    
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : SceneLoader
+{
+    [SerializeField] float timeBeforeNextScene = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GoToNextScene());
+    }
+
+    private IEnumerator GoToNextScene()
+    {
+        yield return new WaitForSeconds(timeBeforeNextScene);
+        LoadNextScene();
+    }
+}
