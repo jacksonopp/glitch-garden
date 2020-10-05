6c55af1c30e36bd7b7ba6def293b359724ed05ae
Jackson Oppenheim <jacksonop@gmail.com>
2020-10-04 21:39:27 -0500

    Redone cactus and lizard animation

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StarDisplay : MonoBehaviour
+{
+
+    [SerializeField] int stars = 100;
+    TextMeshProUGUI starText;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        starText = GetComponent<TextMeshProUGUI>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int amount)
+    {
+        if (stars >= 0)
+        {
+            stars -= amount;
+            UpdateDisplay();
+        }
+
+    }
+}
